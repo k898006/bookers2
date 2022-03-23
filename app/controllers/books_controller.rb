8780706de4de15342a3fb07054c0530d3a9da9d9
EC2 Default User <ec2-user@ip-172-31-32-238.ec2.internal>
2022-03-23 11:12:05 +0000

    [finish]bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+
+  def create
+    @user = current_user
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Operation was successfully completed"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def show
+    @book_detail = Book.find(params[:id])
+    @user = current_user
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Operation was successfully completed"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
